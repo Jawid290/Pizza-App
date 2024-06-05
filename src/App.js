e111deb47edc6cf553adddb387809b17aa8bff2c
Jawid <Jawid.hashemi290@gmail.com>
2024-06-05 16:16:53 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
+  // const hour = new Date().getHours();
+  // const openHour = 8;
+  // const closeHour = 20;
+  // const isOpen = hour >= openHour && hour <= closeHour;
+  // console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're cueently opned");
+  // else alert("Sorry. We're currebtly closed");
+  // console.log(hour);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Menu />
+      <Pizza />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h1>Pizza Spinaci</h1>
+      <h1>Ingredients:Tomato, mozarella, spinach, and ricotta cheese </h1>
+      <img src="./pizzas/spinaci.jpg" alt=" spinici" />
+    </div>
+  );
+}
+
+function Footer() {
+  <h2>This is the footer</h2>;
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 20;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We'er Currently open
+    </footer>
   );
 }
 
